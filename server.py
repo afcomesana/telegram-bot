0cf2d2f442bcb771894d1e41901f4a1e5a03bef9
afcomesana <albertofcomesana@gmail.com>
2022-12-12 23:09:08 +0100

    use open ai gpt3 instead of predefined answers

--- server.py
+++ server.py
@@ -1,49 +1,56 @@
+import os
+import re
+import unidecode
+import openai
 from flask import Flask, request, jsonify
 from bot_modules.telegram_bot import TelegramBot
-import unidecode
 
+openai.api_key = os.getenv('OPENAI_API_KEY')
 server = Flask(__name__)
 
 @server.post('/')
 def handle_telegram_request():
     
-    # Receive request and prepare response for Telegram:
-    data             = request.json
-    webhook_response = (b'{}', 200)
-    
-    # Provissionaly, only response process 'message' info
-    if 'message' not in data.keys(): return webhook_response
-    
-    message = data['message']
-    
-    # Initialize response dict, always gonna have the chat id and the parse is gonna be HTML:
-    response = {
-        'chat_id': message['chat']['id'],
-        'parse_mode': 'HTML'
-    }
-    
-    # The message is sent from a group, then we reply to that specific message:
-    if message['chat']['type'] == 'group':
-        # If the message is from a group, only response if we are mentioned
-        if special_key_word_to_listen_for not in message['text'].lower(): return webhook_response
-        response['reply_to_message_id'] = message['message_id']
+    try:
         
-    # The message is from private chat, prepare an answer consisting provissionaly on a greeting plus de user name:
-    response['text'] = "Hola%s" % (' %s.' % message['from']['first_name'] if 'first_name' in message['from'].keys() else '.')
-    
-    # If we want a response depending on a name from a specific friend (not the best solution):
-    predefined_answers = {
-        'name1': 'Response for user with name1',
-        'name2': 'Response for user with name2',
-        'name3': 'Response for user with name3',
-        'name4': 'Response for user with name4'
-    }
-    match_name = list(filter(lambda name: name in unidecode.unidecode(message['from']['first_name'].lower()), predefined_answers.keys()))
-    if len(match_name) == 1:
-        match_name = unidecode.unidecode(match_name[0])
-        response['text'] += ' ' + predefined_answers[match_name]
+        # Receive request and prepare response for Telegram:
+        data             = request.json
+        webhook_response = (b'{}', 200)
         
-    bot = TelegramBot()
-    bot.send_message(response)
+        # Provissionaly, only response process 'message' info
+        if 'message' not in data.keys(): return webhook_response
+        
+        message = data['message']
+        
+        # Initialize response dict, always gonna have the chat id and the parse is gonna be HTML:
+        response = {
+            'chat_id': message['chat']['id'],
+            'parse_mode': 'HTML'
+        }
 
-    return webhook_response
+        # The message is sent from a group, then we reply to that specific message:
+        if message['chat']['type'] == 'group':
+            # If the message is from a group, only response if we are mentioned
+            if not re.search(r'tu(m|n)(n|m)?us', message['text'].lower()):
+                return webhook_response
+            
+            response['reply_to_message_id'] = message['message_id']
+            
+        openai_response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=re.sub(r'(señor)? ?tu(m|n)(n|m)?us', 'tú', message['text'], flags=re.IGNORECASE),
+            temperature=0.9,
+            frequency_penalty=0.0,
+            max_tokens=500,
+            presence_penalty=0.6
+        )
+        
+        response['text'] = openai_response['choices'][0]['text']
+            
+        bot = TelegramBot()
+        bot.send_message_beta(response)
+
+        return webhook_response
+    
+    finally:
+        return webhook_response
